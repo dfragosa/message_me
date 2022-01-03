b22a46c66b0ae3defaab7391d36a8d293e09d59d
dfragosa <fderric@gmail.com>
2022-01-03 15:16:17 -0500

    Add semantic-ui gem, jquery, popper.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+// added from their github
+const webpack = require('webpack')
+
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
